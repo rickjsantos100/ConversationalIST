52bb9f0144fa5e1ce8dbfbbe76d25faac9712756
Andre <33375224+Andree37@users.noreply.github.com>
2022-06-02 20:45:10 +0100

    signin with new username

--- app/src/main/java/pt/ulisboa/tecnico/cmov/conversationalist/activities/SignInActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/conversationalist/activities/SignInActivity.java
@@ -1,26 +1,75 @@
 package pt.ulisboa.tecnico.cmov.conversationalist.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import pt.ulisboa.tecnico.cmov.conversationalist.R;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import pt.ulisboa.tecnico.cmov.conversationalist.databinding.ActivitySignInBinding;
+import pt.ulisboa.tecnico.cmov.conversationalist.models.User;
+import pt.ulisboa.tecnico.cmov.conversationalist.utilities.PreferenceManager;
 
 public class SignInActivity extends AppCompatActivity {
 
-    private static final String TAG = "Home Activity";
-    private static final String USER_OBJECT = "user_object";
+    private ActivitySignInBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
 
-        String user = (String) getIntent().getSerializableExtra(USER_OBJECT);
-        if (user != null) {
-            Log.d(TAG, user);
+    private void setListeners() {
+        binding.buttonSignIn.setOnClickListener(v -> {
+            String username = binding.inputUsername.getText().toString().trim();
+            if (username.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Enter a username", Toast.LENGTH_SHORT).show();
+            } else {
+                loading(true);
+                FirebaseFirestore database = FirebaseFirestore.getInstance();
+                database.collection("users").document(username).get().addOnCompleteListener(task -> {
+                    if (task.isSuccessful() && task.getResult() == null) {
+                        loading(false);
+                        Toast.makeText(getApplicationContext(), "Username already exists", Toast.LENGTH_SHORT).show();
+                    } else {
+                        database.collection("users").document(username).set(new User(System.currentTimeMillis())).addOnCompleteListener(tt -> {
+                            if (tt.isSuccessful()) {
+                                // we got a new user and we can start it
+                                DocumentSnapshot document = task.getResult();
+                                assert document != null;
+                                preferenceManager.putBoolean("signedIn", true);
+                                preferenceManager.putString("username", username);
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                            } else {
+                                loading(false);
+                                Toast.makeText(getApplicationContext(), "Username already exists", Toast.LENGTH_SHORT).show();
 
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    private void loading(boolean isLoading) {
+        if (isLoading) {
+            binding.buttonSignIn.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        } else {
+            binding.buttonSignIn.setVisibility(View.VISIBLE);
+            binding.progressBar.setVisibility(View.INVISIBLE);
         }
     }
 }
